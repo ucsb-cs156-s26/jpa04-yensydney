a9f590d5fe30519577116e3b5d09c29c1def0e27
Nini Yen <h_yen@ucsb.edu>
2026-04-15 12:36:30 -0700

    correct the output

--- src/main/java/jpa04/Hello.java
+++ src/main/java/jpa04/Hello.java
@@ -2,6 +2,6 @@ package jpa04;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("This is the wrong output!");
+        System.out.println("Hello, World!");
     }
 }
